3599529aac1506782c9ec234834cd8988d26f89e
LW7373 <96071346+LW7373@users.noreply.github.com>
2023-09-21 14:46:41 -0400

    Create MeatTypeEnum.java

---
+++ MeatTypeEnum.java
@@ -0,0 +1,17 @@
+/*
+Moses Dong, Nicholas Xu, and Lindsay Wang
+Schenk
+AP CSA - Period 7
+Glizzy Goblin
+19 September 2023
+*/
+
+package dongwangxu.seven;
+
+public class MeatTypeEnum {
+    
+    // Create enum for meat type
+    public enum MeatType{
+        beef, pork, chicken, venison, turkey, vegan, lamb, mystery
+    }
+}
