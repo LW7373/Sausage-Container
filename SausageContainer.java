5b7e696e5679c4cf44969ee5c0b81312a72fdd9e
MD3729 <111062288+MD3729@users.noreply.github.com>
2023-09-22 15:27:40 -0400

    Template

--- SausageContainer.java
+++ SausageContainer.java
@@ -15,27 +15,51 @@ public class SausageContainer {
     // Class fields
     private Sausage[] sausageArray;
 
-    // Default constructor - Lindsay
+
+    // Default constructor - 
     public SausageContainer(){
-        for (int a = 0; a < 100; a++){
+
+    }
+
+    // Partial Constructor
+    public SausageContainer(int a){
+        //Must have attributes if its oiwn
+        //Sausage array
+
+    }
+
+    // Full Constructor
+    public SausageContainer(int a, int b){
+
+    }
+
+    //Default fill method
+    private void fillArray(){
+        for (int a = 0; a < 100; a++){//Whatever this is may not work
             sausageArray[a] = new Sausage();
         }
     }
 
-    private Sausage ReturnSausage (int a, int b){
+
+    //CRUD
+    private void allSausages(){
+        
+    }
+
+    private Sausage returnSausage (int a, int b){
         //return sausageArray[a][b];
         return new Sausage();
     }
 
-    private void ChangeSausage(int a, int b, Sausage newSausage){
+    private void changeSausage(int a, int b, Sausage newSausage){
 
     }
 
-    private void AddSausage(int endingIndex, Sausage newSausage){
+    private void addSausage(int endingIndex, Sausage newSausage){
 
     }
 
-    private void clearSausage(int a, int b){
+    private void clearSausage(int a, int b){ //Delete Sausage
         //sausageArray[a][b] = new Sausage(); 
     }
 
